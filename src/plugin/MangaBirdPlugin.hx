b7f0ffcb6668ccfdc02b9f75f90f95ff6c9d1b25
elnabo <g.desquesnes@gmail.com>
2014-12-12 18:47:35 +0100

    new plugin for mangabird

--- src/plugin/MangaBirdPlugin.hx
+++ src/plugin/MangaBirdPlugin.hx
@@ -2,35 +2,27 @@ package plugin;
 
 import haxe.Http;
 
-class MangaFoxPlugin extends Plugin
+class MangaBirdPlugin extends Plugin
 {
-	public static var mainURL(default,null):String = "http://mangafox.me/manga/";
+	public static var mainURL(default,null):String = "http://www.mangabird.com/";
 	override private function set_manga(value:String):String
 	{
-		return super.set_manga(StringTools.trim(value).toLowerCase().split(" ").join("_"));
+		return super.set_manga(StringTools.trim(value).toLowerCase().split(" ").join("-"));
 	}
-	private static var imgRegex:EReg = new EReg("<img.*?\"(http.*?)\"","");
+	private static var imgRegex:EReg = new EReg("<img.*?\"(http://image.mangabird.*?)\"","");
 	
 	public function new(?manga:String=null)
 	{
 		super(manga);
+		trace(manga);
 	}
 	
 	override public function getImageURL(chap:Int, page:Int):String
 	{
-		var url:String = mainURL + manga + "/c" + StringTools.lpad(""+chap,"0",3) + "/" + page + ".html";
-		var request:Http = new Http(url);
-		request.setHeader("User-Agent", "Mozilla/5.0");
-		request.onStatus = function (i:Int)
-			{
-				if (i != 200)
-				{
-					throw "Http Error #404";
-				}
-			};
-		request.request();
-		
-		var doc = request.responseData;
+		var url:String = mainURL + manga + "-" + chap + "?page=" + page;
+		var doc:String = Http.requestUrl(url);
+		if (doc.indexOf("Page not found") != -1)
+			throw "Http Error #404";
 		
 		try
 		{
@@ -46,8 +38,9 @@ class MangaFoxPlugin extends Plugin
 	override public function doesChapterExists(chap:Int):Bool
 	{
 		try
-		{			
-			return (getImageURL(chap, 1) != "");
+		{
+			var doc:String = Http.requestUrl(mainURL+manga+"-"+chap);
+			return (doc.indexOf("Page not found") == -1);
 		}
 		catch ( _ : Dynamic)
 		{
@@ -58,7 +51,7 @@ class MangaFoxPlugin extends Plugin
 	
 	override public function exists():Bool
 	{
-		var h = new Http(mainURL+manga);
+		var h = new Http(mainURL+manga+"-1");
 		var exist = true;
 		h.onError = function(e:Dynamic) 
 			{
